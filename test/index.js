cfe43b7a6b6af8325933bfa465350b6c51bd61d9
Manuel Mujica <manuel.expat@gmail.com>
2018-02-13 16:39:57 -0700

    Create the generator
    Closes #2

--- test/index.js
+++ test/index.js
@@ -1,18 +1,56 @@
+var fs = require('fs');
 var path = require('path');
 var helpers = require('yeoman-test');
 var assert = require('yeoman-assert');
 
 describe('donejs-travis-to-firebase', function() {
-  before(function(done) {
-    helpers.run(path.join(__dirname, '../default'))
-      .inTmpDir()
-      .withPrompts({
-        name: 'testing'
-      }).on('end', done);
+  describe('without .travis.yml', function() {
+    before(function(done) {
+      helpers
+        .run(path.join(__dirname, '../default'))
+        .inTmpDir()
+        .on('end', done);
+    });
+
+    it('does not write travis.yml', function() {
+      assert.noFile('.travis.yml');
+    });
   });
 
-  it('should write testing.js file', function() {
-    assert.file(['testing.js']);
-    assert.fileContent('testing.js', /This is a file from the donejs-travis-to-firebase DoneJS generator/);
+  describe('travis.yml has before_deploy steps', function() {
+    before(function(done) {
+      helpers
+        .run(path.join(__dirname, '../default'))
+        .inTmpDir(function(dir) {
+          fs.copyFileSync(
+            path.join(__dirname, 'travis_before_deploy_fixture.yml'),
+            path.join(dir, '.travis.yml')
+          );
+        })
+        .on('end', done);
+    });
+
+    it('does not override travis.yml before_deploy steps', function() {
+      assert.fileContent('.travis.yml', /echo 'ready\?'/);
+    });
+  });
+
+  describe('with travis.yml without before_deploy steps', function() {
+    before(function(done) {
+      helpers
+        .run(path.join(__dirname, '../default'))
+        .inTmpDir(function(dir) {
+          fs.copyFileSync(
+            path.join(__dirname, 'travis_fixture.yml'),
+            path.join(dir, '.travis.yml')
+          );
+        })
+        .on('end', done);
+    });
+
+    it('adds before_deploy steps', function() {
+      assert.fileContent('.travis.yml', /firebase deploy bot/);
+      assert.fileContent('.travis.yml', /npm run deploy:ci/);
+    });
   });
 });
